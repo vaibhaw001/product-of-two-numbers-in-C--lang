a56a14ea8d973e5dd0ac6a96e9b5df5c7b7bd30c
vaibhaw <68787391+vaibhaw001@users.noreply.github.com>
2020-08-07 23:03:22 +0530

    Add files via upload

---
+++ NEW4.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+     int num1,num2,product;
+     clrscr();
+     printf("enter two number: ");
+     scanf("%d %d",&num1,&num2);
+     product=num1*num2;
+     printf("product of two numbers: %d",product);
+     getch();
+     }
